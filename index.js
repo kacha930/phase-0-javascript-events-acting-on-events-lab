b72565dc79dbfaf3c9b2b6ac31961e7de905d267
kacha930 <robiwere20@gmail.com>
2024-07-22 12:02:21 +0300

    moving events

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Your code here
+
+function moveDodgerLeft(){
+    const dodger = document.getElementById('dodger');
+    //style
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+
+    if (left > 0){
+        dodger.style.left = `${left - 10}px`;
+
+    }
+}
+
+//left move then right
+
+function moveDodgerRight(){
+    const dodger = document.getElementById('dodger');
+    let leftNumbers = dodger.style.left.replace('px', '');
+    let left = parseInt(leftNumbers, 10);
+
+    if (left < 360){
+        dodger.style.left =    `${left + 10}px`;
+    }
+}
